5b6d2febd88dcca9140e8de7cb71334a39c31e04
Kaviarasan K K <kaviarasankk@gmail.com>
2017-06-27 10:39:39 +0530

    Re-architect and implemented Mainwindow, treeview, mdiarea, sqlite

---
+++ app.py
@@ -0,0 +1,14 @@
+from relinall.ui import *
+from relinall.db import *
+from relinall.os import *
+
+if __name__ == "__main__":
+       
+	MainWindow()
+	# s= server()
+	# s.getServerData()
+
+	# app = QApplication(sys.argv)
+	# window = Window()
+	# window.show()
+	# sys.exit(app.exec_())
