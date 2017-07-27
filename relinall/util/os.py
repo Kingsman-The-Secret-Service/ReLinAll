e944fac5e80e9acca1a41b58afb24e42a0d7be5e
Albert <albertdevaraj90@gmail.com>
2017-07-27 13:58:57 +0530

    OS/RPM/DEBIAN classes created

--- relinall/util/os.py
+++ relinall/util/os.py
@@ -1,2 +1,42 @@
-class Os(Object):
-	pass
+from relinall.window import *
+
+class Os(Window):
+
+    def __init__(self):
+        ssh = Helper.getData(self.widgetData[self.currentData()['hostname']], 'ssh')
+
+
+	def changeDirectory(self):
+        return 'cd'
+
+    def listDirectory(self):
+        return 'ls'
+
+    def getHostname(self):
+        return 'hostname'
+
+
+class Rpm(Os):
+    pass
+
+class Debian(Os):
+    pass
+
+class Centos(Rpm):
+    pass
+
+class Redhat(Rpm):
+    pass
+
+class Ubuntu(Debian):
+    pass
+
+class Debian(Debian):
+    pass
+
+class Centos6(Centos):
+    pass
+
+class Centos7(Centos):
+    pass
+
